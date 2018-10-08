2be2af809e1431fabba9bd254cd6968dd121b495
kondax <kondaxsolutions@gmail.com>
2018-10-08 01:17:35 +0100

    New UI

--- contracts/TRXMessages.sol
+++ contracts/TRXMessages.sol
@@ -1,21 +1,25 @@
 pragma solidity ^0.4.22;
-contract TRXMessages {
-    
-    constructor() public{
+contract TRXMessages {    
+    constructor() public {
         owner = msg.sender;
     }
     
-    struct Message{
+    struct Message {
         address creator;
         string message;
         uint tips;
         uint tippers;
+        uint time;
     }
 
-    struct Top{
+    struct Top {
         uint id;
         uint tips;
     }
+    
+    event MessageChange (
+       uint id
+    );
 
     uint public current = 0;
     mapping(uint => Message) public messages;
@@ -23,7 +27,7 @@ contract TRXMessages {
     Top[20] public topPosts;
     address owner;
 
-    function tipMessage(uint id) public payable{
+    function tipMessage(uint id) public payable {
         require(msg.value > 100);
 
         Message storage m = messages[id];
@@ -33,45 +37,56 @@ contract TRXMessages {
         uint fee = msg.value / 100;
         uint tip = msg.value - fee;
 
-        if(m.creator.send(tip) && owner.send(fee)){
+        if(m.creator.send(tip) && owner.send(fee)) {
             m.tips += msg.value;
             m.tippers += 1;
             
-            uint256 i  = 0;
-            for(i = 0; i < topPosts.length; i++) {
-                if(topPosts[i].tips < m.tips) {
+            bool found = false;
+            uint smallestIndex = 0;
+            uint smallestValue = ~uint256(0);
+
+            for(uint i = 0;i<topPosts.length;i++) {
+                if(topPosts[i].id == id){
+                    smallestIndex = i;
+                    found = true;
                     break;
                 }
-            }
-            
-            for(uint j = topPosts.length - 1; j > i; j--) {
-                topPosts[j].tips = topPosts[j - 1].tips;
-                topPosts[j].id = topPosts[j - 1].id;
+
+                if(topPosts[i].tips < smallestValue && topPosts[i].tips < m.tips) {
+                    smallestIndex = i;
+                    smallestValue = topPosts[i].tips;
+                    found = true;
+                }
             }
 
-            topPosts[i].tips = m.tips;
-            topPosts[i].id = id;
+            if(found) {
+                topPosts[smallestIndex].tips = m.tips;
+                topPosts[smallestIndex].id = id;
+            }
+
+            emit MessageChange(id);
         }
     }
 
-    function postMessage(string message) public payable returns (uint out) {
+    function postMessage(string message) public payable {
         require(msg.value == 1000000);
-        if(owner.send(1000000)){
-            out = current;
-            messages[out] = Message({
+        
+        if(owner.send(1000000)) {
+            messages[current] = Message({
                 creator: msg.sender,
                 message: message,
                 tips: 0,
-                tippers: 0
+                tippers: 0,
+                time: now
             });
+
+            emit MessageChange(current);
             current++;
-            return out;
         }
     }
 
-    function getMessage(uint id) public view returns (address, string, uint, uint){
+    function getMessage(uint id) public view returns (address, string, uint, uint, uint) {
         Message memory m = messages[id];
-        return (m.creator, m.message, m.tips, m.tippers);
+        return (m.creator, m.message, m.tips, m.tippers, m.time);
     }
-
 }
