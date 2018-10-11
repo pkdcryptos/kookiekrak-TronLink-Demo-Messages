f83a2330f421a07be3284ce16c07078d8d667a5b
metjm <metjm.mail@gmail.com>
2018-10-11 23:04:36 +0200

    add docs

---
+++ docs/precache-manifest.4f1d0d7756ae5e1e43a79458e08b68d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3553e3d40ca90bd44565b6fa013ad4",
+    "url": "/static/media/banner.4c3553e3.png"
+  },
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "/static/media/TronLinkLogo.d3a8f115.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da204bec8c967cf44cc1",
+    "url": "/static/js/main.da204bec.chunk.js"
+  },
+  {
+    "revision": "82a12a027600d4eabbac",
+    "url": "/static/js/1.82a12a02.chunk.js"
+  },
+  {
+    "revision": "da204bec8c967cf44cc1",
+    "url": "/static/css/main.121c85c5.chunk.css"
+  },
+  {
+    "revision": "16800d5cc187c69bd96f316020271ec0",
+    "url": "/index.html"
+  }
+];
