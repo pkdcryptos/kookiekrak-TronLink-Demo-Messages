ae3f5f9636485ad53d7609f3db8159f39a0730c8
athamidn <athamidn@gmail.com>
2020-10-27 11:40:48 +0000

    update and bug fix to newest versions

--- tronbox.js
+++ tronbox.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
 // For trontools/quickstart docker image
-      privateKey: '',
+      privateKey: 'db0c5f0b9bd5d35a8f21c12de56e4e7ecf5b04d452eff963563e1f383e570e60',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
       fullNode: "http://127.0.0.1:8090",
@@ -11,10 +11,11 @@ module.exports = {
       network_id: "*"
     },
     shasta: {
-      privateKey: '',
+      privateKey: 'db0c5f0b9bd5d35a8f21c12de56e4e7ecf5b04d452eff963563e1f383e570e60',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
       fullNode: "https://api.shasta.trongrid.io",
+      fullHost: "https://api.shasta.trongrid.io",
       solidityNode: "https://api.shasta.trongrid.io",
       eventServer: "https://api.shasta.trongrid.io",
       network_id: "*"
@@ -22,7 +23,8 @@ module.exports = {
     mainnet: {
 // Don't put your private key here, pass it using an env variable, like:
 // PK=da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0 tronbox migrate --network mainnet
-      privateKey: process.env.PK,
+//      privateKey: process.env.PK,
+      privateKey: 'db0c5f0b9bd5d35a8f21c12de56e4e7ecf5b04d452eff963563e1f383e570e60',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
       fullNode: "https://api.trongrid.io",
